aa492a59fcdfc44b8bae81c22fccad29e2c61e39
samani siewe darix <samanidarix@gmail.com>
2023-09-26 14:11:24 +0100

    add correct status code for post resquest

--- routes/users.py
+++ routes/users.py
@@ -18,7 +18,7 @@ UserRouter = APIRouter()
 hash_helper = CryptContext(schemes=["bcrypt"])
 
 
-@UserRouter.post("")
+@UserRouter.post("", status_code=status.HTTP_201_CREATED)
 def add_new_user(user: UserCreate):
 
     user_exists = UserNode.match(user.email)
@@ -29,7 +29,7 @@ def add_new_user(user: UserCreate):
     
     user_create = UserNode(email=user.email, fullname=user.fullname, password=hash_helper.encrypt(user.password))
     user_create.create()
-    return HTTPException(status_code=status.HTTP_201_CREATED, detail="user successfully created")
+    return user_create
 
 
 @UserRouter.post("/login")
@@ -61,7 +61,9 @@ def user_get_token(user_credentiel: OAuth2PasswordRequestForm = Depends() ):
         raise HTTPException(status_code=403, detail="Incorrect email or password")
     
     raise HTTPException(status_code=403, detail="Incorrect email or password")
-@UserRouter.delete("/{user_uuid}")
+
+
+@UserRouter.delete("/{user_uuid}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user_uuid: UUID, user: UserNode = Depends(get_current_user)):
 
     if not user:
